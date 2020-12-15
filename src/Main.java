f0d6c47a9f41805349ffef754857552f0e4ebf6c
Tyler <Tylerpower1551@gmail.com>
2020-12-15 00:32:47 +0000

    cleaned up code

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+    private static ArrayList<String> windows = new ArrayList<>();
+    private static WebDriver driver = new ChromeDriver();
 
     public static void main(String[] args) throws InterruptedException {
-//       Create arraylist to store windows (facebook login etc)
-        ArrayList<String> windows = new ArrayList<>();
-
-//        Set up chrome driver
+//        Set chromedriver location and access website
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("http://www.tinder.com/");
 
-//        Select login buttons and choose facebook login
+//        Call login function
+        login(driver);
+
+//        Find like button
+        WebElement like = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button"));
+
+//        Auto swipe function
+        auto_swipe(driver, like);
+
+    }
+
+    public static void login(WebDriver driver) throws InterruptedException {
+//        Find and select login buttons and choose facebook login
         WebElement login = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/button/span"));
         login.click();
+
         Thread.sleep(500);
+
         WebElement fb_log = driver.findElement(By.xpath("//*[@id=\"modal-manager\"]/div/div/div[1]/div/div[3]/span/div[2]/button"));
         fb_log.click();
 
@@ -32,10 +40,11 @@ public class Main {
         String pass = "";
 
 //        Store all windows in the arraylist and switch the the facebook login
-        for(String currentWindow : driver.getWindowHandles()){
+        for (String currentWindow : driver.getWindowHandles()) {
             driver.switchTo().window(currentWindow);
             windows.add(currentWindow);
         }
+
         Thread.sleep(500);
 
 //        find input fields and send credentials
@@ -55,39 +64,36 @@ public class Main {
         WebElement allow_location = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]"));
         allow_location.click();
         Thread.sleep(500);
+
         WebElement notifications = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]"));
         notifications.click();
+
         WebElement cookies_accept = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/button"));
         cookies_accept.click();
 
-//        Wait to load page
+//        Wait to load main page
         Thread.sleep(5000);
 
-//        Find like button
-        WebElement like = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button"));
+    }
 
-
-//        Infinite swiping, catching exceptions such as tinder popups including Super like, Adding tinder to home page and out of matches
-        while(true){
-            try{
+    public static void auto_swipe(WebDriver driver, WebElement like) throws InterruptedException {
+        while (true) {
+            try {
                 like.click();
-            } catch (Exception e){
-                try{
-                    WebElement supa = driver.findElement(By.xpath("/html/body/div[2]/div/div/button[2]"));
-                    supa.click();
-                } catch (Exception ioe){
+            } catch (Exception e) {
+                try {
+                    driver.findElement(By.xpath("/html/body/div[2]/div/div/button[2]")).click();
+                } catch (Exception ioe) {
                     driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button[2]")).click();
                     try{
                         like.click();
-                    }
-                    catch (Exception out_of_matches){
-                        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div/main/div/div[1]/div/div[1]/div[3]/button")).click();
+                    } catch (Exception match){
+                        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/main/div[2]/div/div/div[1]/div/div[4]/button"));
                     }
                 }
-
             }
+
             Thread.sleep(250);
         }
-
     }
 }
